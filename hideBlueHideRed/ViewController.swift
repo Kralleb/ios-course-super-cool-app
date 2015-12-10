683d24a60f2d23c9cf183dd0b60126ea78850a7e
Kaeleb Rocha <Kralleb@Kaelebs-MacBook-Pro.local>
2015-12-09 18:08:38 -0800

    Added files for hideBlueHideRed

--- hideBlueHideRed/ViewController.swift
+++ hideBlueHideRed/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueBg: UIImageView!
+    @IBOutlet weak var redBg: UIImageView!
+    @IBOutlet weak var showRedButton: UIButton!
+    @IBOutlet weak var showBlueButton: UIButton!
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +29,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func showRedButtonPressed(sender: AnyObject) {
+        
+        showBlueButton.hidden = false
+        showRedButton.hidden = true
+        redBg.hidden = false
+        blueBg.hidden = true
+        
+    }
+    @IBAction func showBlueButtonPressed(sender: AnyObject) {
+        
+        showBlueButton.hidden = true
+        showRedButton.hidden = false
+        redBg.hidden = true
+        blueBg.hidden = false
+        
     }
 
 
